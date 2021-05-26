78325700147d84c6f464e52103466b5c4bdbf1eb
Adriano Tumino <adrianotumino@gmail.com>
2021-05-26 15:24:03 +0200

    Rifattorizzata classe CatchNews

--- app/src/main/java/it/adriano/tumino/gamepoint/news/CatchNews.java
+++ app/src/main/java/it/adriano/tumino/gamepoint/news/CatchNews.java
@@ -1,5 +1,6 @@
 package it.adriano.tumino.gamepoint.news;
 
+import android.util.Log;
 import android.view.View;
 
 import org.jsoup.Jsoup;
@@ -20,17 +21,25 @@ import it.adriano.tumino.gamepoint.ui.news.NewsViewModel;
 import it.adriano.tumino.gamepoint.utils.TaskRunner;
 
 public class CatchNews extends TaskRunner<Integer, List<News>> {
+    public static final String TAG = "CatchNews";
+
+    private static final String urlEveryeye = "https://www.everyeye.it/notizie/?pagina=";
+    private static final String urlMultiplayer = "https://multiplayer.it/articoli/notizie/?page=";
+
+    private static final String[] mounth = {"Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"};
 
     private final NewsViewModel newsViewModel;
     private final NewsAdapter newsAdapter;
 
-    public CatchNews(NewsViewModel newsViewModel, NewsAdapter newsAdapter){
+    public CatchNews(NewsViewModel newsViewModel, NewsAdapter newsAdapter) {
+        Log.i(TAG, "Creazione Catch News");
         this.newsViewModel = newsViewModel;
         this.newsAdapter = newsAdapter;
     }
 
     @Override
     public List<News> doInBackground(Integer... integers) {
+        Log.i(TAG, "Prelevamento Notizie");
         ArrayList<News> list = getEveryeye(integers[0]);
         list.addAll(getMultiplayer(integers[0]));
         Collections.shuffle(list);
@@ -39,73 +48,79 @@ public class CatchNews extends TaskRunner<Integer, List<News>> {
 
     @Override
     public void onPostExecute(List<News> gameNewsList) {
+        Log.i(TAG, "Inserimento Notizie");
         newsViewModel.getShimmerFrameLayout().stopShimmer();
         newsViewModel.getShimmerFrameLayout().setVisibility(View.GONE);
         newsViewModel.getRecyclerView().setVisibility(View.VISIBLE);
 
         newsViewModel.setList(gameNewsList);
-        newsAdapter.notifyDataSetChanged(); //nuovo
+        newsAdapter.notifyDataSetChanged();
     }
 
     private ArrayList<News> getEveryeye(int page) {
-        String url = "https://www.everyeye.it/notizie/?pagina=" + page;
+        String url = urlEveryeye + page;
         ArrayList<News> list = new ArrayList<>();
         try {
-            Document document = Jsoup.connect(url).get();
+            Document document = getFromUrl(url);
+
             Elements elements = document.getElementsByClass("fvideogioco");
             for (Element element : elements) {
                 String imageURL = element.getElementsByClass("lazy").get(0).attributes().get("data-src"); //nullo se non è possibile trovare il tag
 
-                Element notizia = element.getElementsByClass("testi_notizia").get(0); //dati della notizia
-                String data = notizia.getElementsByTag("span").get(0).text().split(",")[1].substring(1); //prendo solo la data, senza lo spazio iniziale
+                Element news = element.getElementsByClass("testi_notizia").get(0); //dati della notizia
+                String date = news.getElementsByTag("span").get(0).text().split(",")[1].substring(1); //prendo solo la data, senza lo spazio iniziale
 
-                Element link = notizia.getElementsByTag("a").get(0); //link notizia e titolo
-                String notiziaUrl = link.attributes().get("href");
-                String titolo = link.attributes().get("title");
-                String testo = notizia.getElementsByTag("p").get(0).text();
+                Element link = news.getElementsByTag("a").get(0); //link notizia e titolo
+                String newsUrl = link.attributes().get("href");
+                String title = link.attributes().get("title");
+                String body = news.getElementsByTag("p").get(0).text();
 
-                News gameNews = new News(titolo, testo, imageURL, data, notiziaUrl, "everyeye.it");
+                News gameNews = new News(title, body, imageURL, date, newsUrl, "everyeye.it");
                 list.add(gameNews);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(TAG, "Errore Prelevamento Notizie Everyeye: " + e.getMessage());
         }
         return list;
     }
 
     private ArrayList<News> getMultiplayer(int page) {
-        String[] mesi = {"Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"};
-        String url = "https://multiplayer.it/articoli/notizie/?page=" + page;
+        Log.i(TAG, "Prelevamento Notizie da Multiplayer");
+        String url = urlMultiplayer + page;
         ArrayList<News> list = new ArrayList<>();
         try {
-            Document document = Jsoup.connect(url)
-                    .userAgent("Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36") //per visualizzare tutto correttamente
-                    .get();
+            Document document = getFromUrl(url);
 
             Elements elements = document.getElementsByClass("media");
             for (Element element : elements) {
                 String imageURL = element.getElementsByTag("img").get(0).attributes().get("data-src"); //nullo se non è possibile trovare il tag
                 Element mediaBody = element.getElementsByClass("media-body").get(0);
                 String newsUrl = "https://multiplayer.it" + mediaBody.getElementsByTag("a").get(0).attributes().get("href");
-                String titolo = mediaBody.getElementsByTag("a").get(0).text();
+                String title = mediaBody.getElementsByTag("a").get(0).text();
                 String[] subString = mediaBody.getElementsByTag("p").text().split("\\|");
-                String testo = subString[subString.length - 1].trim();
-                String data = subString[0].split(" - ")[subString[0].split(" - ").length - 1];
+                String body = subString[subString.length - 1].trim();
+                String date = subString[0].split(" - ")[subString[0].split(" - ").length - 1];
 
-                if(!data.matches("([0-9]{2})\\\\([0-9]{2})\\\\([0-9]{4})")){
+                if (!date.matches("([0-9]{2})\\\\([0-9]{2})\\\\([0-9]{4})")) {
                     SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
                     String[] string = sdf.format(Calendar.getInstance().getTime()).split("/");
-                    data = string[0] + " " + mesi[Integer.parseInt(string[1]) - 1] + " " + string[string.length - 1];
+                    date = string[0] + " " + mounth[Integer.parseInt(string[1]) - 1] + " " + string[string.length - 1];
                 }
-                if(!testo.isEmpty()){
-                    News gameNews = new News(titolo, testo, imageURL, data, newsUrl, "multiplayer.it");
+                if (!body.isEmpty()) {
+                    News gameNews = new News(title, body, imageURL, date, newsUrl, "multiplayer.it");
                     list.add(gameNews);
                 }
-
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e(TAG, "Errore Prelevamento Notizie Multiplayer: " + e.getMessage());
         }
         return list;
+    }
+
+    private Document getFromUrl(String url) throws IOException {
+        Document document = Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")
+                .get();
+        return  document;
     }
 }
