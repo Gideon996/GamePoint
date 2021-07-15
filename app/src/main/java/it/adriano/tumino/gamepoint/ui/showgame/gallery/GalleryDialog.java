9a3051f83c7d83d81a35880b01a5bd5ec6d6a156
Adriano Tumino <adrianotumino@gmail.com>
2021-07-15 18:48:56 +0200

    Ora la galleria funziona correttamente

--- app/src/main/java/it/adriano/tumino/gamepoint/ui/showgame/gallery/GalleryDialog.java
+++ app/src/main/java/it/adriano/tumino/gamepoint/ui/showgame/gallery/GalleryDialog.java
@@ -1,16 +1,21 @@
 package it.adriano.tumino.gamepoint.ui.showgame.gallery;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 import androidx.viewpager2.widget.ViewPager2;
 
 import java.util.List;
+import java.util.Objects;
 
 import it.adriano.tumino.gamepoint.R;
 import it.adriano.tumino.gamepoint.adapter.vievpager.ImageGalleryAdapter;
@@ -19,11 +24,18 @@ public class GalleryDialog extends DialogFragment {
 
     private final int initialPosition;
     private final List<String> screenshots;
+    private final ImageView[] dots;
+
+    private Drawable dot;
+    private Drawable sferaDelDrago;
+
     public GalleryDialog(int initialPosition, List<String> screenshots) {
         this.initialPosition = initialPosition;
         this.screenshots = screenshots;
+        dots = new ImageView[screenshots.size()];
     }
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle bundle) {
@@ -36,10 +48,42 @@ public class GalleryDialog extends DialogFragment {
         viewPager2.setAdapter(new ImageGalleryAdapter(screenshots));
         viewPager2.setCurrentItem(initialPosition);
 
+        dot = getResources().getDrawable(R.drawable.ic_pallina, requireContext().getTheme());
+        sferaDelDrago = getResources().getDrawable(R.drawable.sfera_del_drago, requireContext().getTheme());
+
+        setIndicators(view);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                selectedDots(position);
+                super.onPageSelected(position);
+            }
+        });
+
+
         Button chiudi = view.findViewById(R.id.chiudiBottone);
-        chiudi.setOnClickListener(k -> getDialog().dismiss());
+        chiudi.setOnClickListener(k -> Objects.requireNonNull(getDialog()).dismiss());
 
         return builder.create();
+    }
+
+    private void selectedDots(int position) {
+        dots[position].setImageDrawable(sferaDelDrago);
+        if (position - 1 >= 0) dots[position - 1].setImageDrawable(dot);
+        if (position + 1 != screenshots.size()) dots[position + 1].setImageDrawable(dot);
+    }
+
+    private void setIndicators(View view) {
+        LinearLayout linearLayout = view.findViewById(R.id.dots_container);
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new ImageView(getContext());
+            dots[i].setImageDrawable(dot);
+            if (i == 0) dots[i].setImageDrawable(sferaDelDrago);
+            linearLayout.addView(dots[i]);
+        }
+
     }
 
 
